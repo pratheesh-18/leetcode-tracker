e476386592a6a2dce2048417bbf882f9fa921094
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:36:39 +0530

    Create 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions

---
+++ 1724-CustomerWhoVisitedButDidNotMakeAnyTransactions/1724-CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -0,0 +1,8 @@
+-- Last updated: 7/8/2026, 3:36:41 PM
+/* Write your T-SQL query statement below */
+SELECT 
+        customer_id,
+        COUNT(customer_id) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (SELECT visit_id FROM Transactions)
+GROUP BY customer_id
