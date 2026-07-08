74c4552b3cd7fd31f2f9e87710fd59ef6bf536db
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:38:44 +0530

    Create 777-ToeplitzMatrix

---
+++ 777-ToeplitzMatrix/777-ToeplitzMatrix.java
@@ -0,0 +1,13 @@
+// Last updated: 7/8/2026, 3:38:46 PM
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
