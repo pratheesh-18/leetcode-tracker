49ea3383ac1aa74829c616fd1aacc8471f098e8e
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:37:35 +0530

    Create 1338-QueriesQualityAndPercentage

---
+++ 1338-QueriesQualityAndPercentage/1338-QueriesQualityAndPercentage.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/8/2026, 3:37:37 PM
+SELECT
+    query_name,
+    ROUND(SUM(rating / position) / COUNT(*), 2) AS quality,
+    ROUND(
+        SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*),
+        2
+    ) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
