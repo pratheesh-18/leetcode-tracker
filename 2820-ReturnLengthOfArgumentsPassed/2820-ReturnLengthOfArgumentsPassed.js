5c10e5de2a2e725b129e56ea91b8caa7819abe9e
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:33:59 +0530

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,13 @@
+// Last updated: 7/8/2026, 3:34:01 PM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let count =args.length
+    return count
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
