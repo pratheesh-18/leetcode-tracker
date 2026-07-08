1ad99a721eb0a79360441ad31e526d38e9a6e631
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:43:16 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.js
@@ -0,0 +1,30 @@
+// Last updated: 7/8/2026, 3:43:19 PM
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right =nums.length-1
+    
+    
+    while(left<=right){
+         let middle = Math.floor((left + right) / 2);
+        if(nums[middle]==target){
+            return middle
+        }
+        else if(nums[middle]>target){
+            right =middle-1
+        }
+        else{
+
+            left=middle+1
+        }
+        
+
+    }
+    return left
+   
+    
+};
