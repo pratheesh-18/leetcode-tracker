a48890d40e32a27368625fcc375b52a72d7837c4
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:36:03 +0530

    Create 2083-ThreeDivisors

---
+++ 2083-ThreeDivisors/2083-ThreeDivisors.java
@@ -0,0 +1,13 @@
+// Last updated: 7/8/2026, 3:36:05 PM
+class Solution {
+    public boolean isThree(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0) count++;
+            if(count>3) return false;
+        }
+        if(count<3) return false;
+        else return true;
+        
+    }
+}
