185152a944accba8464028bb7c88adf4f579242c
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:41:53 +0530

    Create 124-BinaryTreeMaximumPathSum

---
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.java
@@ -0,0 +1,20 @@
+// Last updated: 7/8/2026, 3:41:55 PM
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        int[] res = { root.val };
+        dfs(root, res);
+        return res[0];
+    }
+
+    private int dfs(TreeNode node, int[] res) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftSum = Math.max(0, dfs(node.left, res));
+        int rightSum = Math.max(0, dfs(node.right, res));
+
+        res[0] = Math.max(res[0], leftSum + rightSum + node.val);
+        return Math.max(leftSum, rightSum) + node.val;
+    }    
+}
