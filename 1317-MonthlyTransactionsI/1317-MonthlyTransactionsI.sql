ce03e67526901c62e1b33f95b2f6aab57c6c29b3
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:37:41 +0530

    Create 1317-MonthlyTransactionsI

---
+++ 1317-MonthlyTransactionsI/1317-MonthlyTransactionsI.sql
@@ -0,0 +1,12 @@
+-- Last updated: 7/8/2026, 3:37:44 PM
+SELECT 
+    LEFT(trans_date, 7) AS month,
+    country, 
+    COUNT(id) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM((state = 'approved') * amount) AS approved_total_amount
+FROM 
+    Transactions
+GROUP BY 
+    month, country;
