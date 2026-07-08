d8d0bc859817c97cb9beac27e4d4b04a0999e4ca
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:36:30 +0530

    Create 1806-CountOfMatchesInTournament

---
+++ 1806-CountOfMatchesInTournament/1806-CountOfMatchesInTournament.java
@@ -0,0 +1,20 @@
+// Last updated: 7/8/2026, 3:36:32 PM
+class Solution {
+    public int numberOfMatches(int n) {
+        // int sum=0;
+        // while(n>1){
+        //      if(n%2==0) {
+        //         sum+=n/2;
+        //         n=n/2;
+        //         }
+        //      else{
+        //          sum+=(n-1)/2;
+        //          n=((n-1)/2)+1;
+        //      }
+        // }
+        // return sum;
+
+        return n-1;
+        
+    }
+}
