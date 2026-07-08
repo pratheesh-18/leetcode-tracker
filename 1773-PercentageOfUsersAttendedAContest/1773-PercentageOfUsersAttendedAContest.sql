90f9e4d9684707a3ee9fcdcb2844f56765cc99b9
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:36:37 +0530

    Create 1773-PercentageOfUsersAttendedAContest

---
+++ 1773-PercentageOfUsersAttendedAContest/1773-PercentageOfUsersAttendedAContest.sql
@@ -0,0 +1,7 @@
+-- Last updated: 7/8/2026, 3:36:39 PM
+select 
+contest_id, 
+round(count(distinct user_id) * 100 /(select count(user_id) from Users) ,2) as percentage
+from  Register
+group by contest_id
+order by percentage desc,contest_id
