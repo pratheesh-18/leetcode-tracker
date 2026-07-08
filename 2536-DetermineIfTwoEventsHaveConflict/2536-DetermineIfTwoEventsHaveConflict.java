29edc2ae843d0ca8bf86d28dcdef014bdecc0352
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-08 15:35:05 +0530

    Create 2536-DetermineIfTwoEventsHaveConflict

---
+++ 2536-DetermineIfTwoEventsHaveConflict/2536-DetermineIfTwoEventsHaveConflict.java
@@ -0,0 +1,9 @@
+// Last updated: 7/8/2026, 3:35:07 PM
+class Solution {
+    public boolean haveConflict(String[] event1, String[] event2) {
+        return event1[0].compareTo(event2[1]) <= 0 &&
+             event2[0].compareTo(event1[1]) <= 0;
+        
+        
+    }
+}
