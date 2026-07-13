4029ead59a71a75f7a8cf64ddeadd910ff73c401
Pratheesh <pratheeshpratheesh2006@gmail.com>
2026-07-13 09:13:29 +0530

    Create 1291-SequentialDigits

---
+++ 1291-SequentialDigits/version/java/1291-SequentialDigits_20260713_091329.java
@@ -0,0 +1,16 @@
+// Last updated: 7/13/2026, 9:13:29 AM
+1class Solution {
+2    public List<Integer> sequentialDigits(int low, int high) {
+3        List<Integer> res=new ArrayList<>();
+4        for(int i=1;i<=9;i++){
+5            int num=i;
+6            for(int j=i+1;j<=9;j++){
+7                num=num*10+j;
+8                if(num>=low && num<=high) res.add(num);
+9            }
+10        }
+11        Collections.sort(res);
+12        return res;
+13        
+14    }
+15}
